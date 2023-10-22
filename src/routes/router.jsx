9ff2e3165dc60ea396680e65a9f9317b16de79e9
Astro <rgrxmahabub.official@gmail.com>
2023-10-23 04:35:38 +0600

    women data created

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,11 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home";
+import Women from "../pages/Women/Women";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
-    children: [{ path: "/", element: <Home /> }],
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/women",
+        element: <Women />,
+      },
+      {},
+    ],
   },
 ]);
--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import { Input } from "@material-tailwind/react";
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [SearchSidebarOpen, setSearchSidebarOpen] = useState(false);
+  const [womenModalOpen, setWomenModalOpen] = useState(false);
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
@@ -21,6 +23,14 @@ const Navbar = () => {
     setSearchSidebarOpen(false);
   };
 
+  const openWomenModal = () => {
+    setWomenModalOpen(true);
+  };
+
+  const closeWomenModal = () => {
+    setWomenModalOpen(false);
+  };
+
   return (
     <nav className="fixed top-0 left-0 right-0 w-full z-50">
       <div className="text-black flex justify-between items-center py-16 md:px-16">
@@ -32,9 +42,30 @@ const Navbar = () => {
           >
             ☰
           </button>
-          {/* Conditionally render Item 1, Item 2, and Item 3 in the left section for md screens */}
           <div className="hidden md:flex gap-6 items-center text-sm capitalize">
-            <div className="hover:border-b-2  border-black">Women</div>
+            <Link to={"/women"}>
+              <div
+                className="hover:border-b-2 border-black cursor-pointer"
+                onMouseEnter={openWomenModal}
+                onMouseLeave={closeWomenModal}
+              >
+                Women
+                {womenModalOpen && (
+                  <div className="absolute border text-black p-4 mt-1 w-full h-full">
+                    {/* Modal content */}
+                    <div className="hover:border-b-2 duration-100">
+                      Category 1
+                    </div>
+                    <div className="hover:border-b-2 duration-100">
+                      Category 2
+                    </div>
+                    <div className="hover:border-b-2 duration-100">
+                      Category 3
+                    </div>
+                  </div>
+                )}
+              </div>
+            </Link>
             <div className="hover:border-b-2  border-black">Men</div>
             <div className="hover:border-b-2  border-black">Explore</div>
             <div className="hover:border-b-2  border-black">Beauty</div>
@@ -42,9 +73,11 @@ const Navbar = () => {
         </div>
 
         {/* Middle Section (Logo) */}
-        <div className="flex justify-center items-center mr-0 md:mr-[130px]">
-          <img src="/DG-Logo.png" className="w-[70px] h-[70px]" />
-        </div>
+        <Link to={"/"}>
+          <div className="flex justify-center items-center mr-0 md:mr-[130px]">
+            <img src="/DG-Logo.png" className="w-[70px] h-[70px]" alt="Logo" />
+          </div>
+        </Link>
 
         {/* Right Section */}
         <div className="hidden md:flex text-sm">
@@ -65,54 +98,57 @@ const Navbar = () => {
           </div>
           <div className="mr-4 hover:border-b-2  border-black">Cart</div>
         </div>
-
-        {sidebarOpen && (
-          <div className="fixed top-0 left-0  bg-white w-full h-full text-black p-4 transition-transform transform translate-x-0 z-10">
-            {/* Close button for the sidebar */}
-            <div className="mt-10 px-6">
-              <button
-                onClick={closeSidebar}
-                className="absolute top-4 right-4 p-2 text-black text-3xl"
-              >
-                &#215;
-              </button>
-              {/* Sidebar content */}
-              <div className="flex flex-col gap-6 absolute top-[100px]">
-                <div className="hover:border-b-2 duration-100">Women</div>
-                <div className="hover:border-b-2 duration-100">Men</div>
-                <div className="hover:border-b-2 duration-100">Explore</div>
-                <div className="hover:border-b-2 duration-100 border-black">
-                  Beauty
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
-        {SearchSidebarOpen && (
-          <div className="fixed top-0 right-0 w-2/6 h-full bg-white text-black p-4  transition-all transform translate-x-0 z-10">
-            {/* Close button for the sidebar */}
-            <div className="mt-10 px-6">
-              <button
-                onClick={closeSearchSidebar}
-                className="absolute top-4 right-4 p-2 text-black text-sm hover:border-b-2 duration-100 border-black"
-              >
-                Close
-              </button>
-              {/* Sidebar content */}
-              <div className="flex flex-col gap-6 absolute top-[100px] w-3/4 mx-auto px-12">
-                <Input variant="standard" label="Search" />
-                <div className="mt-2 flex flex-col gap-1">
-                  <p className="text-sm mb-4">Trending Searches</p>
-                  <p className="text-sm text-[#737373]">Antigona</p>
-                  <p className="text-sm text-[#737373]">Varsity Jacket</p>
-                  <p className="text-sm text-[#737373]">Shark Lock</p>
-                  <p className="text-sm text-[#737373]">G Tote</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
       </div>
+      {sidebarOpen && (
+        <div className="fixed top-0 left-0 bg-white w-full h-full text-black p-4 transition-transform transform translate-x-0 z-10">
+          {/* Close button for the sidebar */}
+          <div className="mt-10 px-6">
+            <button
+              onClick={closeSidebar}
+              className="absolute top-4 right-4 p-2 text-black text-3xl"
+            >
+              &#215;
+            </button>
+            {/* Sidebar content */}
+            <div className="flex flex-col gap-6 absolute top-[100px]">
+              <div className="hover:border-b-2 duration-100">Women</div>
+              <div className="hover:border-b-2 duration-100">Men</div>
+              <div className="hover:border-b-2 duration-100">Explore</div>
+              <div className="hover:border-b-2 duration-100 border-black">
+                Beauty
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {SearchSidebarOpen && (
+        <div className="fixed top-0 right-0 w-2/6 h-full bg-white text-black p-4 transition-all transform translate-x-0 z-10">
+          {/* Close button for the sidebar */}
+          <div className="mt-10 px-6">
+            <button
+              onClick={closeSearchSidebar}
+              className="absolute top-4 right-4 p-2 text-black text-sm hover-border-b-2 duration-100 border-black"
+            >
+              Close
+            </button>
+            {/* Sidebar content */}
+            <div className="flex flex-col gap-6 absolute top-[100px] w-3/4 mx-auto px-12">
+              <div className="relative">
+                <Input variant="standard" label="Search" />
+                <p className="absolute right-0 bottom-1 text-xs">Sign Up</p>
+              </div>
+
+              <div className="mt-2 flex flex-col gap-1">
+                <p className="text-sm mb-4">Trending Searches</p>
+                <p className="text-sm text-[#737373]">Antigona</p>
+                <p className="text-sm text-[#737373]">Varsity Jacket</p>
+                <p className="text-sm text-[#737373]">Shark Lock</p>
+                <p className="text-sm text-[#737373]">G Tote</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
       <div className="text-center fixed bottom-4 lg:bottom-16 text-2xl font-extrabold w-full z-50 ">
         Divine Glow
       </div>
